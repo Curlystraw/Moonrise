d10eeb65546ee8caddf6b8491c9615c6021778f2
Noah M <scr3amjack@gmail.com>
2016-10-07 14:34:03 -0400

    Implemented Tab Expansion

---
+++ Assets/Scripts/UI scripts/ExpandTemplate.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Treat this script more as an example as to how Large-Menu scripts should be designed.
+ * Keep in mind this script is a component of the panel labelled "Expand"
+ * 
+ * Note: You MUST put everything in OnEnable. Unity throws unfixable errors when
+ * manipulating objects that aren't enabled for some reason. Some sort of 
+ * SUCCEEDED(hr) that crashes the script object. 
+ */
+ 
+public class ExpandTemplate : MonoBehaviour {
+
+    public Canvas canvas; //No idea how important this is. Probably only important for automated UI Movements
+    RectTransform rtf;
+	// Use this for initialization. Start or Awake will crash it. 
+	void OnEnable () {
+        Center(); // While not important to be first, it's best if it is. Centering the window correctly takes little effort and prevents the menu from never appearing due to shenanigans.
+    }
+
+    /// <summary>
+    ///  Force Centers the UI element. 
+    /// </summary>
+    private void Center() //Private because nothing outside this script should be using this.
+    {
+        rtf = gameObject.GetComponent<RectTransform>();
+
+        int width = (int)rtf.rect.width;
+        int height = (int)rtf.rect.height;
+        float x = (Screen.width) / 2; //Anchor of Panel SHOULD be centered. No need to worry.
+        float y = (Screen.height) / 2;
+        Vector3 newpos = new Vector3(x, y);
+        rtf.position = newpos;
+    }
+
+    /* Commented out because superfluous updates eat memory.
+    void Update()
+    {
+        if(gameObject.activeInHierarchy)
+        {
+            //If the UI element needs to repeatedly update: Do it here.
+        }
+    }
+    */
+
+}
--- Assets/Scripts/UI scripts/TabManager.cs
+++ Assets/Scripts/UI scripts/TabManager.cs
@@ -1,10 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
+/* Since this is the main script driving the entire mini-menu design, some documentation here would probably be best on editing in Unity.
+ * The mini-menu is designed to show small menu layouts in the "Contents" panels while allowing the user to "pop out" the menus for their larger variants. 
+ * The overall design is mostly scalable. 
+ * 
+ * ADDING A NEW TAB:
+ * - Resize the TabManager Panel to hold the new tab
+ * - Duplicate one of the tabs. Saves effort over building a new one.
+ * - Rename tab, position it accordingly. 
+ * - Update the tabs array in Unity and add the tab.
+ * - NOTE THAT THE TABS ARRAY IS IN LEFT TO RIGHT ORDER. THIS IS IMPORTANT.
+ * - Go to KeyPoll() and write in a new if statement for the new tab's key.
+ * */
 
 public class TabManager : MonoBehaviour {
 
     private int uiTab;
     public GameObject[] tabs = new GameObject[4]; //This is best modified in the Manager Panel instead of here. Easier and simpler.
+    public GameObject expander;
+    private GameObject expandedTab;
+
+    private bool currentExpansion = false;
 
 	void Start () {  //For init
         uiTab = 0;
@@ -53,15 +71,53 @@ public class TabManager : MonoBehaviour {
     /// </summary>
     public void expandTab()
     {
-        //TODO: Actually write this
-        Debug.Log("Attempt to Expand Tab " + uiTab);
+        if(!currentExpansion) // If no menu is up
+        {
+            Debug.Log("Opening Tab " + uiTab);
+            var expandContents = tabs[uiTab].transform.Find("Expand").gameObject;
+            expandContents.SetActive(true);
+            expandedTab = expandContents;
+            expander.transform.Find("Text").gameObject.GetComponent<Text>().text = ">";
+            currentExpansion = true;
+        }
+        else if(currentExpansion) // If a menu is up
+        {
+            for (int i = 0; i < tabs.Length; i++)
+            {
+                var tabContents = tabs[i].transform.Find("Expand").gameObject;
+                if (tabContents != null)
+                {
+                    if (i == uiTab)
+                    {
+                        if (tabContents.Equals(expandedTab)) // If the menu from the same tab
+                        {
+                            Debug.Log("Closing Tab " + i);
+                            tabContents.SetActive(false);
+                            expandedTab = null;
+                            expander.transform.Find("Text").gameObject.GetComponent<Text>().text = "<";
+                            currentExpansion = false;
+                        }
+                        else // Otherwise
+                        {
+                            Debug.Log("Opening Tab " + i);
+                            expandedTab = tabContents;
+                            tabContents.SetActive(true);
+                        }
+                    }
+                    else tabContents.SetActive(false);
+                }
+            }
+        }
     }
 
-    //Needs to poll keyboard input, since the game is intended for either keyboard or mouse.
-    public void Update()
+    /// <summary>
+    ///  Polls the keyboard for input based on menu keys. 
+    /// </summary>
+    private void KeyPoll()
     {
         //TODO: Find a way to recode keys directly to the tabs, so that hard-coded keys and tab numbers aren't required.
         //TODO: Maybe find a way to simplify this? There has to be a more efficient way.
+        //TODO: Learn Unity keyconfig stuff to allow keyswitching in pre-menu.
         if (Input.GetKeyUp(KeyCode.M))
         {
             if (uiTab != 0)
@@ -90,5 +146,11 @@ public class TabManager : MonoBehaviour {
             else
                 expandTab();
         }
+    }
+
+    //Needs to poll keyboard input, since the game is intended for either keyboard or mouse.
+    public void Update()
+    {
+        KeyPoll();
     }
 }
