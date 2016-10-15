e79c7dd4d055ff1f329735e214db56ae00c65de2
Ryan Kann <rak3me@d-172-25-203-209.dhcp.virginia.edu>
2016-10-15 14:24:01 -0400

    no message

---
+++ Assets/SlumsGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlumsGenerator : MonoBehaviour {
+
+	[SerializeField]
+	private Transform wallPrefab;
+	[SerializeField]
+	private Transform unwalkablePrefab;
+	[SerializeField]
+	private Transform walkablePrefab;
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+}
