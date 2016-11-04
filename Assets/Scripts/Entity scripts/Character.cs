ac553ebe21d88b28fe3185ac9a64be785df6085d
Austin Chen <senseiaustino@gmail.com>
2016-11-04 18:41:00 -0400

    Begin branch for damage calculation

--- Assets/Scripts/Entity scripts/Character.cs
+++ Assets/Scripts/Entity scripts/Character.cs
@@ -41,6 +41,11 @@ namespace Completed
 			inventory = new Inventory ();
 		}
 
+		public void Attack(Character enemy)
+		{
+			
+		}
+
 		/// <summary>
 		/// Add the item to the inventory.
 		/// </summary>
