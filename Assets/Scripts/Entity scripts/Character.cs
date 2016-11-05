be6502061c93b3cc441e178475549bded592b1a8
Bryan Rombach <bmr3ud@virginia.edu>
2016-11-04 21:08:15 -0400

    added range
    range has been added

--- Assets/Scripts/Entity scripts/Character.cs
+++ Assets/Scripts/Entity scripts/Character.cs
@@ -11,6 +11,7 @@ namespace Completed
 		private double baseBlock;
 		private int baseAttack;
 		private double baseAccuracy;
+		private int baseRange;
 
 		//affected by items
 		private int totalHP;
@@ -18,19 +19,24 @@ namespace Completed
 		private double totalBlock;
 		private int totalAttack;
 		private double totalAccuracy;
+		private int totalRange;
 
 		private int currentHP;
+
+		private double baseSpeed;
+		private double totalSpeed;
+
 
 		private EquippedItemSet equippedItems;
 		private Inventory inventory;
 
 		Random rand = new Random();
 
-		public Character () : this(100, .1, .1, 5, .9)
+		public Character () : this(100, .1, .1, 5, .9, 5, 1.0)
 		{
 		}
 
-		public Character (int hp, double dodge, double block, int attack, double accuracy)
+		public Character (int hp, double dodge, double block, int attack, double accuracy,int range, double speed)
 		{
 			baseHP = hp;
 			baseBlock = block;
@@ -45,7 +51,11 @@ namespace Completed
 			equippedItems = new EquippedItemSet ();
 			inventory = new Inventory ();
 		}
-
+		/// <summary>
+		/// Ranged attack function
+		/// </summary>
+		/// <returns>The attack.</returns>
+		/// <param name="target">Target.</param>
 		public int RangedAttack(Character target) {
 			if (rand.NextDouble () <= this.TotalAccuracy) {
 				if (rand.NextDouble () > target.TotalDodge) {
@@ -56,7 +66,11 @@ namespace Completed
 			}
 			return 0;
 		}
-
+		/// <summary>
+		/// Melee attack function
+		/// </summary>
+		/// <returns>The attack.</returns>
+		/// <param name="target">Target.</param>
 		public int MeleeAttack(Character target) {
 			if (rand.NextDouble () <= this.TotalAccuracy) {
 				if (rand.NextDouble () > target.TotalBlock) {
@@ -75,6 +89,16 @@ namespace Completed
 		public void AddItem(Item item)
 		{
 			inventory.AddItem (item);
+		}
+
+		/// <summary>
+		/// Remove the item from the inventory.
+		/// </summary>
+		/// <returns><c>true</c>, if item was removed, <c>false</c> otherwise.</returns>
+		/// <param name="item">Item.</param>
+		public bool RemoveItem(Item item)
+		{
+			return inventory.RemoveItem (item);
 		}
 
 		/// <summary>
@@ -106,17 +130,6 @@ namespace Completed
 		{
 			Item unequipped = equippedItems.Unequip (ic);
 			AddItem (unequipped);
-		}
-			
-
-		/// <summary>
-		/// Remove the item from the inventory.
-		/// </summary>
-		/// <returns><c>true</c>, if item was removed, <c>false</c> otherwise.</returns>
-		/// <param name="item">Item.</param>
-		public bool RemoveItem(Item item)
-		{
-			return inventory.RemoveItem (item);
 		}
 
 		#region properties	
